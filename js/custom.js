35b8e770bccc7e0117175fc895fcc3b000597571
nhuuhuy <nguyenhuuhuy02@gmail.com>
2017-04-13 21:28:45 +0700

    update 8

--- js/custom.js
+++ js/custom.js
@@ -1,37 +1,37 @@
-jQuery(document).ready(function($){
-	$(".nav li").each(function(){
-		$(this).click(function(){
-			$(".nav li").removeClass("active");
-			$(this).addClass("active")
-		})
-	})
-	$(".search").click(function(){
-		$("#search").fadeToggle('slow')
-	})
-	
-	$('.multi-item-carousel .item').each(function(){
-			  var next = $(this).next();
-			  if (!next.length) {
-			    next = $(this).siblings(':first');
-			  }
-			  next.children(':first-child').clone().appendTo($(this));
+jQuery(document).ready(function($) {
+    $(".nav li").each(function() {
+            $(this).click(function() {
+                $(".nav li").removeClass("active");
+                $(this).addClass("active")
+            })
+        })
+        // $(".search").click(function(){
+        // 	$("#search").fadeToggle('slow')
+        // })
 
-			  
-			  if (next.next().next().next().length>0 ) {
-			    next.next().children(':first-child').clone().appendTo($(this));
-			    
-			  } else {
-			  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-			  }
-			});
+    $('.multi-item-carousel .item').each(function() {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
 
-	$("#img_01").elevateZoom({gallery:'gal1', cursor: 'pointer', galleryActiveClass: 'zoom', imageCrossfade: true,}); 
 
-		//pass the images to Fancybox
-		$("#img_01").bind("click", function(e) {  
-		  var ez =   $('#img_01').data('elevateZoom');	
-			$.fancybox(ez.getGalleryList());
-		  return false;
-		});
+        if (next.next().next().next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+
+        } else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
+
+    $("#img_01").elevateZoom({ gallery: 'gal1', cursor: 'pointer', galleryActiveClass: 'zoom', imageCrossfade: true, });
+
+    //pass the images to Fancybox
+    $("#img_01").bind("click", function(e) {
+        var ez = $('#img_01').data('elevateZoom');
+        $.fancybox(ez.getGalleryList());
+        return false;
+    });
 
 })
